ae81e72dff632bbcb76ab53fc19560287982c20a
Shlomi Kushchi <shlomik@gmail.com>
2020-02-25 11:22:04 +0200

    install requirements

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ setup(
     platforms=['any'],
     keywords='pandas, yahoo finance, pandas datareader',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
-    install_requires=['pandas>=0.24', 'numpy>=1.15',
-                      'requests>=2.20', 'multitasking>=0.0.7', 'feedparser==5.2.1', 'lxml==4.5.0'],
+    install_requires=['pandas==0.22.0', 'numpy==1.15.4',
+                      'requests>=2.20', 'multitasking==0.0.9', 'feedparser==5.2.1'],
     entry_points={
         'console_scripts': [
             'sample=sample:main',
